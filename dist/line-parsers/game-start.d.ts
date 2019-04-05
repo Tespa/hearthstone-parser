6f09be482bc7cba3579033057d072069d203c7d7
Alex Van Camp <vancamp.alex@gmail.com>
2019-04-05 14:51:23 -0500

    chore: stop tracking dist folder
    The contents of this folder are published as builds, but are not committed to source control.

--- dist/line-parsers/game-start.d.ts
+++
@@ -1,9 +0,0 @@
-import { AbstractLineParser } from './AbstractLineParser';
-import { GameState } from '../GameState';
-export declare class GameStartLineParser extends AbstractLineParser {
-    regex: RegExp;
-    eventName: string;
-    lineMatched(_parts: string[], gameState: GameState): void;
-    formatLogMessage(_parts: string[], _gameState: GameState): string;
-    shouldEmit(_gameState: GameState): boolean;
-}
