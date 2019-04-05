363bcfb15ce0e5f7e1edaee932742ee0660db050
Alex Van Camp <vancamp.alex@gmail.com>
2019-04-05 14:51:23 -0500

    chore: stop tracking dist folder
    The contents of this folder are published as builds, but are not committed to source control.

--- dist/line-parsers/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { GameOverLineParser } from './game-over';
-import { GameStartLineParser } from './game-start';
-import { MulliganStartLineParser } from './mulligan-start';
-import { NewPlayerLineParser } from './new-player';
-import { TurnLineParser } from './turn';
-import { ZoneChangeLineParser } from './zone-change';
-import { TagChangeLineParser } from './tag-change';
-export declare const lineParsers: (GameOverLineParser | GameStartLineParser | MulliganStartLineParser | NewPlayerLineParser | TurnLineParser | ZoneChangeLineParser | TagChangeLineParser)[];
--- dist/line-parsers/tag-change.d.ts
+++
@@ -1,9 +0,0 @@
-import { AbstractLineParser } from './AbstractLineParser';
-import { GameState } from '../GameState';
-export declare class TagChangeLineParser extends AbstractLineParser {
-    regex: RegExp;
-    eventName: string;
-    lineMatched(parts: string[], gameState: GameState): void;
-    formatLogMessage(parts: string[], _gameState: GameState): string;
-    shouldEmit(_gameState: GameState): boolean;
-}
